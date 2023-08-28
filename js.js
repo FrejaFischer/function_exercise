87cda9b67c37fc05c15d9c7e017f51c76e1a1209
Freja Fischer <frej657e@stud.kea.dk>
2023-08-28 18:00:11 +0200

    Function exercise

---
+++ js.js
@@ -0,0 +1,63 @@
+"use strict";
+//exercise 1
+// function greeting(firstName) {
+//   return `Hello ${firstName}`;
+// }
+
+// const sayHi = greeting;
+
+// console.log(sayHi("Harry"));
+
+// console.log(greeting("Ron"));
+
+//exercise 2
+// const person3 = {
+//   firstName: "Harry",
+//   lastName: "Potter",
+//   hired: false,
+// };
+// const person4 = {
+//   firstName: "Fred",
+//   lastName: "Weasley",
+//   hired: false,
+// };
+
+// function hire(person) {
+//   person.hired = true;
+// }
+
+// function fire(person) {
+//   person.hired = false;
+// }
+
+// function fireOrHire(action, person) {
+//   action(person);
+// }
+
+// fireOrHire(hire, person4);
+// fireOrHire(fire, person3);
+
+// console.log(person3);
+// console.log(person4);
+
+//exercise 3
+
+function loadJSON(url, callback) {
+  fetch(`${url}`)
+    .then((response) => response.json())
+    .then(callback);
+}
+
+function init() {
+  console.log("init");
+
+  loadJSON("marvel.json", prepareData);
+}
+
+function prepareData(data) {
+  console.log("prepare data");
+
+  console.table(data);
+}
+
+init();
