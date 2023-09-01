ef08230b4027e51d7d6a9d4501a70240077e97af
Freja Fischer <frej657e@stud.kea.dk>
2023-09-01 08:29:36 +0200

    new viden

--- js.js
+++ js.js
@@ -11,53 +11,53 @@
 // console.log(greeting("Ron"));
 
 //exercise 2
-// const person3 = {
-//   firstName: "Harry",
-//   lastName: "Potter",
-//   hired: false,
-// };
-// const person4 = {
-//   firstName: "Fred",
-//   lastName: "Weasley",
-//   hired: false,
-// };
+const person3 = {
+  firstName: "Harry",
+  lastName: "Potter",
+  hired: false,
+};
+const person4 = {
+  firstName: "Fred",
+  lastName: "Weasley",
+  hired: false,
+};
 
-// function hire(person) {
-//   person.hired = true;
-// }
+function hire(person) {
+  person.hired = true;
+}
 
-// function fire(person) {
-//   person.hired = false;
-// }
+function fire(person) {
+  person.hired = false;
+}
 
-// function fireOrHire(action, person) {
-//   action(person);
-// }
+function fireOrHire(action, person) {
+  action(person);
+}
 
-// fireOrHire(hire, person4);
-// fireOrHire(fire, person3);
+fireOrHire(hire, person4);
+fireOrHire(fire, person3);
 
-// console.log(person3);
-// console.log(person4);
+console.log(person3);
+console.log(person4);
 
 //exercise 3
 
-function loadJSON(url, callback) {
-  fetch(`${url}`)
-    .then((response) => response.json())
-    .then(callback);
-}
+// function loadJSON(url, callback) {
+//   fetch(`${url}`)
+//     .then((response) => response.json())
+//     .then(callback);
+// }
 
-function init() {
-  console.log("init");
+// function init() {
+//   console.log("init");
 
-  loadJSON("marvel.json", prepareData);
-}
+//   loadJSON("marvel.json", prepareData);
+// }
 
-function prepareData(data) {
-  console.log("prepare data");
+// function prepareData(data) {
+//   console.log("prepare data");
 
-  console.table(data);
-}
+//   console.table(data);
+// }
 
-init();
+// init();
